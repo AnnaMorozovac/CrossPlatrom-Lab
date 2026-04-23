850434aba06546edeaa505968409445d5d509fe0
AnnaMorozovac <morozova.anna@chnu.edu.ua>
2026-04-23 23:28:38 +0300

    [13] Updated Web (laba 5)

--- laba/composeApp/src/webMain/kotlin/ua/morozova/laba/main.kt
+++ laba/composeApp/src/webMain/kotlin/ua/morozova/laba/main.kt
@@ -3,6 +3,15 @@ package ua.morozova.laba
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import ua.morozova.laba.ui.shared_mobile.main.MainScreen
+import kotlin.js.ExperimentalWasmJsInterop
+import kotlin.js.JsModule
+
+
+@OptIn(ExperimentalWasmJsInterop::class)
+@JsModule("@js-joda/timezone")
+external object JsJodaTimeZoneModule
+
+private val jsJodaTz = JsJodaTimeZoneModule
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
